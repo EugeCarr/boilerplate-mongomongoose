cc1c0a3e40c24de2564716dfb4f9070f5a71f75d
EugeCarr <eugecarr@gmail.com>
2023-10-25 20:02:59 +0100

    first mongoose model

--- myApp.js
+++ myApp.js
@@ -3,7 +3,16 @@ mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let personSchema = new mongoose.Schema({
+  "name": {
+    "type": String,
+    "required": true
+  },
+  "age": Number,
+  favoriteFoods: [String]
+})
+
+let Person = mongoose.model("Eugene", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
